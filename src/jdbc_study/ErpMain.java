653cb59455a53efd2c0592c42c80daeb75aabbde
변지영 <bjy8668@hanmail.net>
2019-06-21 15:26:53 +0900

    원래는 자세하게 적어야 됩니다아아아아아 Main 수정

--- src/jdbc_study/ErpMain.java
+++ src/jdbc_study/ErpMain.java
@@ -20,6 +20,7 @@ public class ErpMain {
 		
 		//EmployeeDaoTest안에 getImage()함수에서 사용하는 이미지 경로 확인하려고 적은거
 		//System.out.println(System.getProperty("user.dir"));
-		//System.out.println(System.getProperty("user.dir")+System.getProperty("file.separator"));
+		System.out.println(System.getProperty("user.dir")+System.getProperty("file.separator"));
+		System.out.println("첫번째수정");
 	}
 }
