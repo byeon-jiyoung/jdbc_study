f5167e21cd484db214d0286a9c11852bb73684fe
변지영 <bjy8668@hanmail.net>
2019-06-21 16:34:59 +0900

    DeptEmpTransactionDaoImpl 작업 구현 완료

--- src/jdbc_study/dao/DeptEmpTransactionDao.java
+++ src/jdbc_study/dao/DeptEmpTransactionDao.java
@@ -6,3 +6,5 @@ import jdbc_study.dto.Employee;
 public interface DeptEmpTransactionDao {
 	int trInsertEmpAndDept(Employee emp, Department dept);
 }
+//새로 추가할 때 마다 commit을 해주는게 유지보수 차원에서 좋다.
+//다 하고 한번에 commmit을 하면 나중에 수정을 하고 싶을 때 싹 다 없어짐
